084ce035c964fcba4d1b8a1512cafdcb3e5483d3
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-04-20 12:45:50 +0530

    Update the Start time, End time and Starting date

--- src/js/index.js
+++ src/js/index.js
@@ -8,10 +8,15 @@ const stopBtnEl = document.getElementById("stop-btn")
 
 const taskTitleEl = document.getElementById("task-title")
 const totaldurationEl = document.getElementById("total-duration")
+const fromToTillEl = document.getElementById("from-to-till")
+const taskDateEl = document.getElementById("task-date")
 
 let hours = 0
 let minutes = 0
 let seconds = 0
+
+let startTime = null
+let endTime = null
 
 const updateTimer = () => {
   
@@ -24,8 +29,7 @@ const updateTimer = () => {
     hours++
   }
 
-  const updatedTimer = `
-    ${(hours < 10) ? "0" + hours : hours}:${(minutes < 10) ? "0" + minutes : minutes}:${(seconds < 10) ? "0" + seconds : seconds}`
+  const updatedTimer = `${(hours < 10) ? "0" + hours : hours}:${(minutes < 10) ? "0" + minutes : minutes}:${(seconds < 10) ? "0" + seconds : seconds}`
 
   timerEl.innerText = updatedTimer
 }
@@ -33,6 +37,9 @@ const updateTimer = () => {
 let timerInstance = null
 
 startBtnEl.addEventListener("click", () => {
+  // Store the task start time
+  startTime = new Date()
+
   trackerTitleEl.innerText = inputEl.value
   timerEl.innerText = initialTimeEl.innerText
   timerInstance = setInterval(updateTimer, 1000)
@@ -40,8 +47,18 @@ startBtnEl.addEventListener("click", () => {
 })
 
 stopBtnEl.addEventListener("click", () => {
+  // Store the task ending time
+  endTime = new Date()
+
+  // Clear the Interval 
   clearInterval(timerInstance)
+
   taskTitleEl.innerText = trackerTitleEl.innerText
   totaldurationEl.innerText = timerEl.innerText
   timerEl.innerText = initialTimeEl.innerText
+  
+  taskDateEl.innerText = `${startTime.toDateString()}`
+  fromToTillEl.innerText = 
+  `${startTime.getHours()}:${startTime.getMinutes()} - ${endTime.getHours()}:${endTime.getMinutes()}
+  `
 })
